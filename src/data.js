3ffd4b8689d2d998f1aac5296cac2115fa92c783
Arup Mondal <mondalconnect17@gmail.com>
2022-11-22 18:58:28 +0530

    data

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'Do I have to allow the use of cookies?',
+    info:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iste consequuntur laudantium cumque odio praesentium ea omnis tempora doloremque impedit. Non neque expedita corporis? Error, beatae animi! Hic modi aliquam enim repellat! Natus, maiores?',
+  },
+  {
+    id: 2,
+    title: 'How do I change my My Page password?',
+    info:
+      'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+  },
+  {
+    id: 3,
+    title: 'What is BankID?',
+    info:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iste consequuntur laudantium cumque odio praesentium ea omnis tempora doloremque impedit. Non neque expedita corporis? Error, beatae animi! Hic modi aliquam enim repellat! Natus, maiores?',
+  },
+  {
+    id: 4,
+    title: 'Whose birth number can I use?',
+    info:
+      'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+  },
+  {
+    id: 5,
+    title: 'When do I recieve a password ordered by letter?',
+    info:
+      'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+  },
+]
+export default questions
